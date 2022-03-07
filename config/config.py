150b9dea3f5da5bfa2311defdffadb757f3abfea
Venom❤️Candy <97267165+CandyMusic@users.noreply.github.com>
2022-03-07 08:27:01 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/CandyMusic/YukkiMusicBot",
+    "https://github.com/CandyMusic/Alisha-Stream",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
