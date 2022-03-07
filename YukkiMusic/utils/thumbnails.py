ba79ec6ffdc50e3f545e97db6f9665c9ee8f64ce
Venom❤️Candy <97267165+CandyMusic@users.noreply.github.com>
2022-03-07 07:58:30 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -94,7 +94,7 @@ async def gen_thumb(videoid):
         )
         draw.text(
             (600, 150),
-            "NOW PLAYING",
+            "ABHIMANYU RANAWAT",
             fill="white",
             stroke_width=2,
             stroke_fill="white",
